432ad6898d99127be153613f77ce6310c1b6597f
purba17 <37559904+purba17@users.noreply.github.com>
2018-08-01 17:00:54 -0400

    Update Main.java

--- src/main/java/json2csv/Main.java
+++ src/main/java/json2csv/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		String filePath = "D:\\ZaloniWork\\parsertoconvertjsontocsvformat\\zdp_entity_complex_2.json";
+		String filePath = "";
 		Json2CSV j2c = new Json2CSV();
 		j2c.readJson(filePath);
 		j2c.createEntityList();
